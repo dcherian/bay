b9edd6986373cfd3537cd7313af85958ea2ae5af
dcherian <deepak@cherian.net>
2019-04-15 17:03:26 -0600

    misc files.

---
+++ makesubmission.bash
@@ -0,0 +1,20 @@
+fname=paper1
+
+# org2latex $fname.org
+
+hash=$(git log -n 1 --pretty=%H)
+
+pdflatexstr='pdflatex --shell-escape %O %S && exiftool -overwrite_original -Producer='$hash' %B.pdf'
+echo $pdflatexstr
+
+if [ $? == 0 ]; then
+    latexmk -gg -pvc -latex="$pdflatexstr"  -pdf $fname.tex;
+fi
+exiftool -overwrite_original -Producer='$hash' $fname.pdf
+
+if xdotool search --name "$fname.pdf -" key r; then
+    echo "PDF updated."
+else
+   exo-open $fname.pdf
+   echo "PDF opened."
+fi
